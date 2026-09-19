1907961647fa28560f522c4f7bf20b21105a332d
hasib <piratehasib@gmail.com>
2026-09-19 22:02:24 +0600

    fix api url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-	baseURL: import.meta.env.MODE === 'development' ? 'http://localhost:5000/api' : `${import.meta.env.VITE_API_URL}`,
+	baseURL: import.meta.env.MODE === 'development' ? 'http://localhost:5000/api' : `${import.meta.env.VITE_API_URL}/api`,
 	withCredentials: true
 })
--- frontend/src/lib/socket.js
+++ frontend/src/lib/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client'
 
-const apiUrl = import.meta.env.MODE === 'development' ? 'http://localhost:5000/api' : `${import.meta.env.VITE_API_URL}`
+const apiUrl = import.meta.env.MODE === 'development' ? 'http://localhost:5000/api' : `${import.meta.env.VITE_API_URL}/api`
 
 export const socket = io(apiUrl, {
 	withCredentials: true
